0866b130107f518cb7c1d67cf531b0273e880da6
Shane Becker <veganstraightedge@gmail.com>
2025-10-15 03:42:33 -0700

    Bundle gem

---
+++ lib/numruby/stats.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "stats/version"
+
+module Numruby
+  module Stats
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/numruby/stats_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Numruby::Stats do
+  it "has a version number" do
+    expect(Numruby::Stats::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
